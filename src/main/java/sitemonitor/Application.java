58b1712d37a4d2226514e80783c099e8c7300b59
Paul Horn <phorn@hazeldenbettyford.org>
2018-11-28 15:48:55 -0600

    updates

--- src/main/java/sitemonitor/Application.java
+++ src/main/java/sitemonitor/Application.java
@@ -2,7 +2,10 @@ package sitemonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,5 +18,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+	public CacheManager cacheManager() {
+//		GuavaCacheManager cacheManager = new GuavaCacheManager();
+//		cacheManager.setCacheBuilder(CacheBuilder.newBuilder().expireAfterAccess(60, TimeUnit.MINUTES));
+    	CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    	cacheManager.setCacheSpecification("maximumSize=500,expireAfterWrite=60m");
+    	return cacheManager;
+	}     
 
 }
